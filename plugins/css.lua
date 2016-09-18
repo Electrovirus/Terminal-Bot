4e9b8d61a7cb139cbf9147216bfbf2def0d9cc2c
Electrovirus <E.electrovirus@gmail.com>
2016-09-18 22:15:21 +0430

    Create css.lua

---
+++ plugins/css.lua
@@ -0,0 +1,13 @@
+function run(msg, matches)
+text = io.popen("cowsay " .. matches[2]):read('*all')
+ if is_momod(msg) then
+end
+ return text
+end
+return {
+ patterns = {
+'^[!/#](cowsay) (.*)$'
+ }, 
+run = run,
+ moderated = true
+}
